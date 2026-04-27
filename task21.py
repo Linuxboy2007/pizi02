9b9aaf5b861046987b4dfb7f07449e12be42c7cd
user <user@1n405030>
2026-04-27 18:50:27 +0400

    изменена задача 21

--- task21.py
+++ task21.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+n=3
+a = []
+for i in range (n):
+    b = []
+    for j in range (n):
+        print('введите [',i,',',j,'] элемент')
+        b.append(int(input()))
+    a.append(b)
+for i in range (n):
+    for j in range(n):
+        print(a[i][j], end=' ')
+    print()
+maximum=a[0][2]
+for i in range (n):
+    for j in range(n):
+        if maximum<a[i][2]:
+            maximum=a[i][2]
+print('макс в 3 столбце:', maximum)
 
-@author: linux
-"""
-
+maximum=a[1][0]
+for i in range (n):
+    for j in range(n):
+        if maximum<a[1][j]:
+            maximum=a[1][j]
+print('макс во 2 строке:', maximum)
