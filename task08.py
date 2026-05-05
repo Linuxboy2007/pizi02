0ed1e9e8ccfaaa9ea0ffc077dd154610ceb4c1ed
alina758dfg <aizdybayeva@bk.ru>
2026-05-05 18:54:59 +0400

    Изменена задача №8

--- task08.py
+++ task08.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+# Дана целочисленная квадратная матрица размером n*m. Написать программу, позволяющую исключать из нее столбец, в котором расположен минимальный элемент главной диагонали
+n = int(input("Введите размер квадратной матрицы n: "))
 
-@author: linux
-"""
+matrix = []
+print("Введите элементы матрицы построчно (через пробел):")
+for i in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
 
+min_value = matrix[0][0]
+col_index = 0
+
+for i in range(1, n):
+    if matrix[i][i] < min_value:
+        min_value = matrix[i][i]
+        col_index = i
+
+new_matrix = []
+for i in range(n):
+    new_row = []
+    for j in range(n):
+        if j != col_index:
+            new_row.append(matrix[i][j])
+    new_matrix.append(new_row)
+
+print("Минимальный элемент главной диагонали: {min_value} (находится в столбце {col_index})")
+print("Матрица после удаления этого столбца:")
+for row in new_matrix:
+    print(*row)
