6f6a3c33d4489cc75b2f67e1a118bbe6c0b1ae07
ilia005u <ilia005u@gmail.com>
2026-05-19 18:37:54 +0400

    Update task18.py

--- task18.py
+++ task18.py
@@ -7,26 +7,22 @@ Created on Mon Apr 27 08:43:15 2026
 
 n = int(input("Введите размер матрицы n: "))
 
-# Ввод матрицы
 print("Введите матрицу построчно (элементы через пробел):")
 matrix = []
 for i in range(n):
     row = list(map(float, input().split()))
     matrix.append(row)
 
-# Собираем элементы ниже главной диагонали
 elements_below = []
 for i in range(n):
     for j in range(i):
         elements_below.append(matrix[i][j])
 
-# Если нет элементов ниже диагонали (n=1)
 if not elements_below:
     max_below = float('-inf')
 else:
     max_below = max(elements_below)
 
-# Вычисляем сумму нужных элементов
 sum_result = 0
 found = False
 
@@ -36,7 +32,6 @@ for i in range(n):
             sum_result += matrix[i][j]
             found = True
 
-# Вывод результата
 if found:
     print(f"Сумма искомых элементов: {sum_result}")
 else:
