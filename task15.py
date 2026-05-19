3a7a1ac0a69510cdb770ef97b0e1d000e9b906b6
Rishatik225 <risatkasapov770@gmail.com>
2026-05-19 18:53:54 +0400

    Update task15.py

--- task15.py
+++ task15.py
@@ -5,3 +5,28 @@ Created on Mon Apr 27 08:43:15 2026
 @author: linux
 """
 
+
+
+n = int(input("Введите размер матрицы n: "))
+
+print("Введите элементы матрицы:")
+a = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    a.append(row)
+max_diag = abs(a[0][0])
+for i in range(1, n):
+    if abs(a[i][i]) > max_diag:
+        max_diag = abs(a[i][i])
+
+print(f"\nМаксимальный диагональный элемент по модулю: {max_diag}")
+
+for i in range(n):
+    for j in range(0, n, 2):  # только нечетные столбцы (1,3,5... в терминах пользователя)
+        a[i][j] = a[i][j] / max_diag
+      
+print("\nРезультат:")
+for i in range(n):
+    for j in range(n):
+        print(f"{a[i][j]:.2f}", end=" ")
+    print()
