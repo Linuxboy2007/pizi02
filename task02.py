45cafbc8e794a480920d4b538a54475187208d3f
tazzzik777 <t655wr@gmail.com>
2026-05-25 23:26:50 +0400

    task02.py

--- task02.py
+++ task02.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+# Батыркин Никита Владимирович ПИЗИ23о2
+# Дана действительная матрица размером n*m, все элементы которой различны. В каждой строке выбирается элемент с наибольшим значением, затем среди этих чисел выбирается наибольшее. Указать индексы элемента с найденным значением
+def find_max(matrix):
+    max_value = matrix[0][0]
+    row_index = 0
+    col_index = 0
 
-@author: linux
-"""
+    for i in range(len(matrix)):
 
+        row_max = max(matrix[i])
+        j = matrix[i].index(row_max)
+
+        if row_max > max_value:
+            max_value = row_max
+            row_index = i
+            col_index = j
+
+    return max_value, row_index, col_index
+
+
+n = int(input("Введите количество строк: "))
+m = int(input("Введите количество столбцов: "))
+
+matrix = []
+
+for i in range(n):
+    row = list(map(float, input().split()))
+    matrix.append(row)
+
+value, row, col = find_max(matrix)
+
+print("Наибольший элемент:", value)
+print("Индексы:", row, col)
